458c52e2cb60ffa6cf3a4338fff72fdfa2028207
Florent Benoit <fbenoit@redhat.com>
2025-02-14 08:08:21 +0100

    chore: cleanup commented lines
    related to new sonarjs linter
    
    Change-Id: I2394549299b2e58a862fbaf2c669f0cb01caf61e
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- src/app.d.ts
+++ src/app.d.ts
@@ -21,12 +21,7 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-  namespace App {
-    // interface Error {}
-    // interface Locals {}
-    // interface PageData {}
-    // interface Platform {}
-  }
+  namespace App {}
 }
 
 export {};
