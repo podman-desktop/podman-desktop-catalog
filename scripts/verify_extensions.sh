a91c06009ba5d70e79635bec2ee7fbe7c5b7318c
Philippe Martin <phmartin@redhat.com>
2025-12-05 09:23:41 +0100

    ci: add copyright
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- scripts/verify_extensions.sh
+++ scripts/verify_extensions.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-
+#
+# Copyright (C) 2024 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 # This script checks if some version has been added to the extensions.json file
 # and for each version:
 # - if the version is not already present in the repository
